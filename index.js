ab542024077e6e0a2c7a15d2f93185e1761e2636
qbekun <73672498+qbekun@users.noreply.github.com>
2025-09-28 14:16:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import "dotenv/config";
 
-const api = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=hu_tao_%28genshin_impact%29+1girl&api_key=${process.env.API_KEY}&user_id=${process.env.USER_ID}`;
+const api = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=gawr_gura+1girl&api_key=${process.env.API_KEY}&user_id=${process.env.USER_ID}`;
 const seen = [];
 
 cron.schedule("0 8 * * *", async () => {
@@ -14,7 +14,6 @@ cron.schedule("0 8 * * *", async () => {
 	while (true) {
 		post = posts[Math.floor(Math.random() * posts.length)];
 
-    // https://i.redd.it/96gqcmznazrb1.png
 		if (
 			!seen.includes(post.file_url) &&
 			(post.rating === "general" || post.rating === "sensitive")
